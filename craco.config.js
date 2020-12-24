a7f06a0f2ada4a2667180d4f32ec3bf78769666e
Chloe Echikson <chloe.echikson@gmail.com>
2020-12-24 13:33:47 -0500

    Project Setup – TypeScript, TailwindCSS, eslint, gh-pages, ci
    * added typescript
    
    * added tailwind
    
    * added eslint
    
    * added gh pages
    
    * added ci
    
    * renamed files to lowercase to work with ghpages
    
    * removed eslintcache

---
+++ craco.config.js
@@ -0,0 +1,11 @@
+/* eslint-env node */
+module.exports = {
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
+    },
+  },
+}
---
+++ src/worm.tsx
@@ -0,0 +1,7 @@
+import React, { ReactElement } from 'react'
+
+const Worm = (): ReactElement => {
+  return <div></div>
+}
+
+export default Worm
